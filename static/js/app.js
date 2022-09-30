6bed605f5fe4a09dd59e0f1a7ab3314dbf855b5d
Akhil Bandi <akhil.bandi1314@gmail.com>
2022-09-29 19:27:48 -0500

    Added the bar graph.

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,51 @@
 console.log("This is app.js");
 
+//Define a global varibale to hold Url
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
+
 //Draw BarGraph
 
 function DrawBarGraph(sampleId)
 {
     console.log(`DrawBarGraph(${sampleId})`);
+
+    d3.json(url).then(data => {
+        console.log(data);
+
+        let samples = data.samples;
+        let resultArray = samples.filter(s => s.id == sampleId);
+        let result = resultArray[0];
+
+        let otu_ids = result.otu_ids;
+        let otu_labels = result.otu_labels;
+        let sample_values = result.sample_values;
+
+        let yticks = otu_ids.slice(0,10).map(otuId =>  `OTU ${otuId}`).reverse();
+        //console.log(`yticks = ${}`)
+
+        // Create a trace object
+        let barData = {
+            x: sample_values.slice(0,10).reverse(),
+            y: yticks,
+            type: 'bar',
+            text: otu_labels.slice(0,10).reverse(),
+            orientation: 'h'
+
+        };
+        // Put the trace object into array
+        let barArray = [barData];
+
+        let barLayout = {
+            title: "Top 10 Bacteria Cultures Found"
+        }
+
+        //Call the plotly function
+        Plotly.newPlot("bar", barArray, barLayout);
+        
+        
+
+
+    })
 
 }
 
@@ -25,6 +66,19 @@ function ShowMetaData(sampleId)
     
 }
 
+function optionChanged(sampleId)
+{
+    console.log(`optionchanged: (${sampleId})`);
+
+    DrawBarGraph(sampleId);
+    DrawBubbleChart(sampleId);
+    ShowMetaData(sampleId);
+
+
+
+}
+
+
 function InitDashboard()
 {
     console.log("InitDashboard");
@@ -32,7 +86,7 @@ function InitDashboard()
     // Get a handle to the dropdwon
     let selector = d3.select("#selDataset");
 
-    let url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
+
 
     d3.json(url).then(data => {
         console.log("Data:", data);
